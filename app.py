21dd6fc8702b3d6c5fafc9d57c8a9a9a2565c57d
elsie-dev <elcymao@gmail.com>
2020-12-05 20:45:39 -0800

    fix arising bugs in my app.py file

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+
+def main():
+    return render_template('index.html')
+  
+if __name__ =='__main__':  
+    app.run(debug = True)  
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+if __name__ =='__main__':  
+    app.run(debug = True)  
+    
+
+
+
+  
+
+   
