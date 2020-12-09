efd0bb91ba28c816534402af7baf7db112b12cc4
KelvinHalx <kelvinwainaina03@gmail.com>
2020-12-09 16:38:33 +0300

    Update app.py

--- MessengerApp/app.py
+++ MessengerApp/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
