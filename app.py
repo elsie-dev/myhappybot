77b1c3d6e4e0929470d2e55140b82f8db439e8f9
KelvinHalx <kelvinwainaina03@gmail.com>
2020-12-11 09:42:58 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret?'
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(app, debug=True)
