0be3095ffb05ced3ec74c15e6180758270c92224
elsie-dev <elcymao@gmail.com>
2020-12-11 22:43:42 -0800

    add exisiting files

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 app = Flask(__name__)
 
 @app.route("/")
-
-def main():
-    return render_template('index.html')
-  
+def main(): 
+    return 'test'
 if __name__ =='__main__':  
     app.run(debug = True)  
 
-@app.route('/showSignUp')
+@app.route("/showSignUp")
 def showSignUp():
-    return render_template('signup.html')
+    return 'test'
 if __name__ =='__main__':  
     app.run(debug = True)  
     
